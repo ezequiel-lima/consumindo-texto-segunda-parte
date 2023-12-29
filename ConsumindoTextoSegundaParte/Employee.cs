5e6a00a6abdc463046b00567b1140768ed8f7f53
ezequiel-lima <joao.lima94@fatec.sp.gov.br>
2023-12-29 19:39:07 -0300

    Add project files.

---
+++ ConsumindoTextoSegundaParte/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsumindoTextoSegundaParte
+{
+    public class Employee
+    {
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public double Salary { get; private set; }
+    }
+}
---
+++ ConsumindoTextoSegundaParte/Program.cs
@@ -0,0 +1,36 @@
+﻿using ConsumindoTextoSegundaParte;
+using System.Globalization;
+
+Console.Write("Enter full file path: ");
+string path = Console.ReadLine();
+
+if (path is null)
+    return;
+
+using StreamReader file = new StreamReader(path);
+List<Employee> employees = new List<Employee>();
+string? line;
+
+Console.Write("Enter salary: ");
+double salary = double.Parse(Console.ReadLine());
+
+while ((line = file.ReadLine()) is not null)
+{
+    string[] text = line.Split(',');
+    string name = text[0];
+    string email = text[1].Trim();
+    double value = double.Parse(text[2], CultureInfo.InvariantCulture);
+
+    Employee employee = new Employee(name, email, value);
+    employees.Add(employee);
+}
+
+file.Close();
+
+Console.WriteLine($"Email of people whose salary is more than {salary.ToString("F2")}: ");
+employees.OrderBy(x => x.Name).Where(x => x.Salary > salary).ToList().ForEach(x => Console.WriteLine(x.Email));
+
+Console.Write($"Sum of salary of people whose name starts with 'M': " +
+    $"{employees.Where(x => x.Name.StartsWith("M", StringComparison.OrdinalIgnoreCase)).Sum(x => x.Salary).ToString("F2")}");
+
+Console.ReadKey();
